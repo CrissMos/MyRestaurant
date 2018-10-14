d0773a105afe846a770aae9b0dc624f8ed79bbef
CrissMos <cris.hapa@gmail.com>
2018-10-14 03:12:27 +0300

    Initial Controller and Views for Products

---
+++ MyRestaurant/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyRestaurant.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProductsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = _context.Products.ToList();
+
+            return View(products);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var product = _context.Products.SingleOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
+        }
+    }
+}
