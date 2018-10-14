9c9856dd5c4c7ff49e0e80bf8788bd24a644da4f
CrissMos <cris.hapa@gmail.com>
2018-10-14 19:40:28 +0300

    Apply data annotation for Name in Reservation

--- MyRestaurant/Models/Reservation.cs
+++ MyRestaurant/Models/Reservation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,6 +12,8 @@ namespace MyRestaurant.Models
 
         public int Number { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public Table Table { get; set; }
