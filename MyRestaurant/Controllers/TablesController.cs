4fd771219de703e7fbe36f430243cf65bfb426b4
CrissMos <cris.hapa@gmail.com>
2018-10-14 16:59:32 +0300

    Add Initial Controller and Views for Tables p

---
+++ MyRestaurant/Controllers/TablesController.cs
@@ -0,0 +1,42 @@
+﻿using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyRestaurant.Controllers
+{
+    public class TablesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public TablesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Tables
+        public ActionResult Index()
+        {
+            var tables = _context.Tables.ToList();
+
+            return View(tables);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var table = _context.Tables.SingleOrDefault(t => t.Id == id);
+
+            if (table == null)
+                return HttpNotFound();
+
+            return View(table);
+        }
+    }
+}
